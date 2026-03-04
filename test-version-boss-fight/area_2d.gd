b519e0fee5da3544e215398473150b80e26b1df7
beigen1 <beigen1@fordham.edu>
2026-03-03 23:29:32 -0500

    push

---
+++ test-version-boss-fight/area_2d.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+
+
+# @onready var player = $player
+# func _ready():
+	# body_entered.connect(_on_body_entered)
+	
+
+#func _on_body_entered(body):
+	# if body.is_in_group("player"):
+#	body.health -= 1
+#	print(body.health)
+	
---
+++ test-version-boss-fight/test-version-boss-fight 11.15.02 PM/player.gd
@@ -0,0 +1,72 @@
+extends CharacterBody2D
+
+var health = 99
+var facing = Vector2.RIGHT
+const SPEED = 100.0
+# const JUMP_VELOCITY = -400.0
+
+# @export var bullet_scene: PackedScene
+@export var bullet_scene_2: PackedScene
+@onready var bulletspawn = $bulletspawn
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var healthbar = $health_label
+@onready var heal_box = $P1_heal
+
+func fire():
+	var new_bullet = bullet_scene_2.instantiate()
+	# bullet = bullet_scene_2
+	new_bullet.direction = facing
+	new_bullet.shooter = self
+	# print(facing)
+	get_parent().add_child(new_bullet)
+	new_bullet.position = bulletspawn.global_position
+
+func _process(float):
+	if Input.is_action_just_pressed("p1_fire"):
+		fire()
+	healthbar.text = str(health)
+#	if P1_heal.P1_In_Range == true:
+#		print("true")
+
+func _physics_process(delta):
+	# Add the gravity.
+	# if not is_on_floor():
+	#	velocity += get_gravity() * delta
+
+	# Handle jump.
+	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("alt_left", "alt_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var direction2 := Input.get_axis("alt_up", "alt_down")
+	if direction2:
+		velocity.y = direction2 * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	if direction or direction2:
+		facing = Vector2(direction, direction2).normalized()
+
+	#flips sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	#running animation
+	if direction == 0 and direction2 == 0:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("move")
+
+	move_and_slide()
+	
+	if health < 1:
+		queue_free()
+	
+	
